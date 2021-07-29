086b30633031f0cd37a26bf64ee9ce70ff95c511
Adarsh Sadanand Shetty <adarshsadanand7@gmail.com>
2021-07-29 22:38:41 +0530

    fistup

---
+++ src/DefaulTableModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abhi
+ */
+class DefaulTableModel {
+
+    void addRow(String[] data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
